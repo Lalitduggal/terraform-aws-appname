03dfc52ae75b7b288220a64010af41f1fe3bfc73
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 14:13:56 +1000

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -45,3 +45,9 @@ variable "my_public_subnet_route_table_name" {
     default = "test03-pub-rt"
     description = "Name of the public subnet route table"
 }
+
+
+variable "my_public_subnet_route_table_id" {
+    type = string
+    description = "Id of the public subnet route table"
+}
