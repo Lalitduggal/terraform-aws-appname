2afa36445033ed23220d8f3d563c4df62d3c9838
lalit duggal <Lalit.duggal@gmail.com>
2021-09-19 12:25:57 +1000

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -5,3 +5,12 @@ module "my_vpc_module" {
     instance_tenancy = var.instance_tenancy
     vpc_name = var.vpc_name
 }
+
+    
+module "my_vpc_module" {
+    source = "git::https://github.com/Lalitduggal/terraform-aws-modules-monorepo.git//igw"
+
+    my_igw_name       = var.my_igw_name
+    my_igw_vpc_id = var.my_igw_vpc_id
+
+}
